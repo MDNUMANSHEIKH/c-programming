546317b80662833bda82ed63bd4c2e483064b982
MDNUMANSHEIKH <mdnumansheikh171@gmail.com>
2023-12-07 17:19:45 +0600

    tttttttt

---
+++ tttttt.c
@@ -0,0 +1,16 @@
+/*here i gonna print a integer number 
+ thats input get from client or user*/
+
+ #include<stdio.h>
+   int main()
+
+    {
+       int num;
+
+       printf("enter a num1 : ");
+       scanf("%d",&num);
+       printf("you are entered :\n          %d",num);
+
+    getchar();
+
+    }
